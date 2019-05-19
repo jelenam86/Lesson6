ed35d2a0c44317d2b2827f67879d4b6071748e41
jelenam86 <jelena.mat86@gmail.com>
2019-05-19 13:08:24 +0200

    Generate a name for band

--- src/exercises/BandNameGenerator.java
+++ src/exercises/BandNameGenerator.java
@@ -1,5 +1,13 @@
 package exercises;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.Random;
+
 /* 
  * Make 2 ArrayLists; 
  *      1 full of adjectives (e.g. slimy, blind)
@@ -8,5 +16,33 @@ package exercises;
  * When the program is run, one word is randomly selected from each list to generate a band name.
 */
 public class BandNameGenerator {
+	
+	private static ArrayList<String> createList(String path){
+		ArrayList<String> list = new ArrayList<String>();
+		int lines = 0;
+		try {
+			LineNumberReader lnr = new LineNumberReader(new FileReader(new File(path)));
+			lnr.skip(Long.MAX_VALUE);
+			lines = lnr.getLineNumber();
+			lnr.close();
+			BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+			for (int i = 0; i < lines; i++) {
+				list.add(br.readLine());
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public static void main(String[] args) {
+
+		ArrayList<String> adjectives = createList("resource/adjectives");
+		ArrayList<String> nouns = createList("resource/nouns");
+
+		System.out.println("Band Name: " + adjectives.get(new Random().nextInt(adjectives.size())) + " "
+				+ nouns.get(new Random().nextInt(nouns.size())));
+	}
 
 }
