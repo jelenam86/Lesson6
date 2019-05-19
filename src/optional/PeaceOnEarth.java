f9d14eb6313f5ad9b04ec1ef78015358e8b65db3
jelenam86 <jelena.mat86@gmail.com>
2019-05-19 17:33:29 +0200

    Removing items from list

--- src/optional/PeaceOnEarth.java
+++ src/optional/PeaceOnEarth.java
@@ -1,6 +1,7 @@
 package optional;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class PeaceOnEarth {
 	public static void main(String[] args) {
@@ -21,7 +22,22 @@ public class PeaceOnEarth {
 		truth.add('k');
 
 		/* TODO: Remove the hash symbols and print out the truth. */
-		
-		
+
+//		for (int i = 0; i < truth.size(); i++) {
+//			if (truth.get(i).equals('#')) {
+//				truth.remove(i);
+//				i--;
+//			}
+//		}
+
+		Iterator<Character> truthIterator = truth.iterator();
+		while (truthIterator.hasNext()) {
+			Character c = truthIterator.next();
+			if (c.equals('#'))
+				truthIterator.remove();
+		}
+
+		truth.forEach((c) -> System.out.print(c));
+
 	}
 }
