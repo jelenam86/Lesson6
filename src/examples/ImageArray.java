69a9f3e02b60a4aa90756db89172d64539046855
jelenam86 <jelena.mat86@gmail.com>
2019-05-27 13:28:16 +0200

    Added black and white filter

--- src/examples/ImageArray.java
+++ src/examples/ImageArray.java
@@ -13,39 +13,47 @@ public class ImageArray {
 	public static void main(String[] args) {
 
 		ImageViewer imageViewer = new ImageViewer("Amongst The Tales Of Life.jpeg");
-		
+
 		int[][][] imageAsArray = convertToArray(imageViewer.getImage());
-		
+
 		// 1. Print the RGB value for each pixel in the image.
-		
-		for(int[][] pixel : imageAsArray) {
-			for(int[] color : pixel) 
+
+		for (int[][] pixel : imageAsArray) {
+			for (int[] color : pixel)
 				System.out.println(Arrays.toString(color));
 		}
-		
+
 		// 2. Remove all the blue and green from the image.
-		
-		for(int[][] pixel : imageAsArray) {
-			for(int[] color : pixel) {
-				color[1] = 0;
-				color[2] = 0;
-			}
-		}		
-		
+
+//		for (int[][] pixel : imageAsArray) {
+//			for (int[] color : pixel) {
+//				color[1] = 0;
+//				color[2] = 0;
+//			}
+//		}
+
 		// 3. Convert to black & white.
 
-		for(int[][] pixel : imageAsArray) {
-			for(int[] color : pixel) 
-		        	color[2] = color[1] = color[0];
+		// for particular solution, blue and green are not removed
+
+		for (int[][] pixel : imageAsArray) {
+			for (int[] color : pixel) {
+				int luma = (color[0] + color[1] + color[2]) / 3;
+				for (int i = 0; i < 3; i++)
+					color[i] = luma < 128 ? 0 : 255;
+			}
+
 		}
-		
+
 		// [optional] Post a manipulated photo of yourself to Slack.
-		
+
 //		ImageViewer imageViewer2 = new ImageViewer("myPhoto.jpg");
 //		int[][][] imageAsArray2 = convertToArray(imageViewer2.getImage());
-//		for(int[][] pixel : imageAsArray2) {
-//			for(int[] color : pixel) 
-//		        	color[2] = color[1] = color[0];
+//		for (int[][] pixel : imageAsArray2) {
+//			for (int[] color : pixel) {
+//				int luma = (int) Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]);
+//				color[2] = color[1] = color[0] = luma;
+//			}
 //		}
 //		imageViewer2.setImage(convertToImage(imageAsArray2));
 //		imageViewer2.show();
@@ -56,11 +64,11 @@ public class ImageArray {
 //		} catch (IOException e) {
 //			e.printStackTrace();
 //		}
-		
+
 		imageViewer.setImage(convertToImage(imageAsArray));
-		
+
 		imageViewer.show();
-		
+
 	}
 
 	private static int[][][] convertToArray(BufferedImage image) {
